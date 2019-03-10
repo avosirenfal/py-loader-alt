9356366338d587c136f738457aa9d37ee43b51f7
Sirenfal <avosirenfal@gmail.com>
2019-03-10 17:33:29 -0400

    * Switch path for require in webpack example config

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,16 @@
-const path = require('path');
+const require = require('require');
 
 module.exports = {
 	resolve: {
 		alias: {
-			'./org.transcrypt.__runtime__.js': path.resolve(__dirname, 'transcrypt', 'org.transcrypt.__runtime__.js'),
+			'./org.transcrypt.__runtime__.js': require.resolve(__dirname, 'transcrypt', 'org.transcrypt.__runtime__.js'),
 		},
 	},
 	module: {
 		rules: [{
 			test: /\.py$/,
 			use: [{
-				loader: path.resolve("./py-loader-alt.js"),
+				loader: require.resolve("./py-loader-alt.js"),
 			}]
 		}]
 	}
